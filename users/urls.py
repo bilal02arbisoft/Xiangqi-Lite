6689b2fcea33a4eb57406b4a8112514536e2e213
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-08-15 21:32:35 +0500

    configured api refresh custom view and added in url in users

--- users/urls.py
+++ users/urls.py
@@ -2,12 +2,13 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import (SignupView, LogoutView,
                          UserProfileEditView, PasswordChangeView,
-                         UsersListView, UserDeleteView, CustomTokenObtainPairView)
+                         UsersListView, UserDeleteView, CustomTokenObtainPairView,
+                         CustomTokenRefreshView)
 
 
 urlpatterns = [
     path('token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('auth/signup/', SignupView.as_view(), name='signup'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
     path('auth/password_change/', PasswordChangeView.as_view(), name='password-change'),
