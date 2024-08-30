947abf134ebefabe3c35e5363af67d8971caae64
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-08-30 22:35:23 +0500

    game events implemented making sure under single consumer

--- game/serializers.py
+++ game/serializers.py
@@ -1,8 +1,28 @@
 from rest_framework import serializers
+
 from game.models import Game
 
+
 class GameSerializer(serializers.ModelSerializer):
+    game_id = serializers.SerializerMethodField()
+    red_player = serializers.SerializerMethodField()
+    black_player = serializers.SerializerMethodField()
 
     class Meta:
         model = Game
-        fields = '__all__'
+        fields = ('game_id', 'red_player', 'black_player', 'fen', 'initial_fen',
+                  'moves', 'turn', 'status', 'started_at', 'last_updated',
+                  'red_time_remaining', 'black_time_remaining')
+        read_only_fields = ('game_id',)
+
+    def get_game_id(self, obj):
+
+        return obj.get_game_id()
+
+    def get_red_player(self, obj):
+
+        return obj.red_player.username if obj.red_player else None
+
+    def get_black_player(self, obj):
+
+        return obj.black_player.username if obj.black_player else None
--- game/views.py
+++ game/views.py
@@ -1,10 +1,12 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
-from rest_framework import status
 from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from game.models import Game
 from game.serializers import GameSerializer
+from game.utils import hashids
 
 
 class GameCreateView(APIView):
@@ -13,13 +15,13 @@ class GameCreateView(APIView):
     def post(self, request, *args, **kwargs):
 
         game_data = {
-            "red_player": None,
-            "black_player": None,
-            "fen": "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r",
-            "initial_fen": "rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r",
-            "moves": "[]",
-            "turn": "red",
-            "status": "ongoing"
+            'red_player': None,
+            'black_player': None,
+            'fen': 'rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r',
+            'initial_fen': 'rnbakabnr/9/1c5c1/p1p1p1p1p/9/9/P1P1P1P1P/1C5C1/9/RNBAKABNR r',
+            'moves': [],
+            'turn': 'red',
+            'status': 'ongoing'
         }
 
         serializer = GameSerializer(data=game_data)
@@ -30,30 +32,35 @@ class GameCreateView(APIView):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class GameDetailView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, game_id, *args, **kwargs):
+        decoded_id = hashids.decode(game_id)
+        if not decoded_id:
+
+            return Response({'error': 'Invalid game ID'}, status=status.HTTP_404_NOT_FOUND)
+
+        game = get_object_or_404(Game, id=decoded_id[0])
+        serializer = GameSerializer(game)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 class GameUpdateView(APIView):
-    def put(self, request, pk, *args, **kwargs):
-        try:
-            game = Game.objects.get(pk=pk)
-        except Game.DoesNotExist:
+    permission_classes = [IsAuthenticated]
 
-            return Response({'error': 'Game not found'}, status=status.HTTP_404_NOT_FOUND)
+    def put(self, request, game_id, *args, **kwargs):
+        decoded_id = hashids.decode(game_id)
+        if not decoded_id:
+            return Response({'error': 'Invalid game ID'}, status=status.HTTP_404_NOT_FOUND)
+
+        game = get_object_or_404(Game, id=decoded_id[0])
 
         serializer = GameSerializer(game, data=request.data, partial=True)
         if serializer.is_valid():
+
             serializer.save()
 
             return Response(serializer.data, status=status.HTTP_200_OK)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class GameDetailView(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, game_id, *args, **kwargs):
-        game = get_object_or_404(Game, game_id=game_id)
-        serializer = GameSerializer(game)
-
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
