09186919223e6279e89ccda5758b4c05bcb4704e
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-09-04 21:32:06 +0500

    in chat messages returning user_id

--- game/consumers.py
+++ game/consumers.py
@@ -5,13 +5,16 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 from game.handlers import game_handler
 
 
-class GameConsumer(AsyncWebsocketConsumer):
+class XiangqiConsumer(AsyncWebsocketConsumer):
+    """Handles WebSocket connections for real-time  events."""
+
     def __init__(self, *args, **kwargs):
+        """Initialize the consumer with the default settings."""
         super().__init__(*args, **kwargs)
         self.lobby_group_name = None
 
     async def connect(self):
-
+        """Accepts or rejects an incoming WebSocket connection."""
         user = self.scope['user']
         if user.is_authenticated:
 
@@ -22,10 +25,10 @@ class GameConsumer(AsyncWebsocketConsumer):
             )
             await self.accept()
         else:
-
             await self.close()
 
     async def disconnect(self, close_code):
+        """Removes the WebSocket connection from the group upon disconnection."""
         if self.lobby_group_name:
 
             await self.channel_layer.group_discard(
@@ -34,18 +37,18 @@ class GameConsumer(AsyncWebsocketConsumer):
             )
 
     async def receive(self, text_data=None, bytes_data=None):
+        """Processes messages received from the WebSocket."""
         data = json.loads(text_data)
         event_type = data.get('type')
-
         if event_type.startswith('game'):
 
             await game_handler.route_event(self, data)
 
     async def send_message(self, event):
+        """Sends a message to the WebSocket, excluding certain channels if specified."""
         exclude_channel = event.get('exclude_channel', None)
         if exclude_channel and exclude_channel == self.channel_name:
 
             return
-
         message_data = event['message_data']
         await self.send(text_data=json.dumps(message_data))
--- game/routing.py
+++ game/routing.py
@@ -1,6 +1,7 @@
 from django.urls import re_path
-from game.consumers import GameConsumer
+
+from game.consumers import XiangqiConsumer
 
 websocket_urlpatterns = [
-    re_path(r'ws/game/$', GameConsumer.as_asgi()),
+    re_path(r'ws/game/$', XiangqiConsumer.as_asgi()),
 ]
