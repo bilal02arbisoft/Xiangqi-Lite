8367b05935bff14fe364f8bccdae4ca844cd2cca
Bilal Ahmad <bilal.ahmad02@A006-01020.local>
2024-09-10 19:07:07 +0500

    improved formatting and docstrings

--- game/views.py
+++ game/views.py
@@ -9,11 +9,21 @@ from game.serializers import GameSerializer
 from game.utils import hashids
 
 
-class GameCreateView(APIView):
+class BaseAPIView(APIView):
+    """
+    Base API view that includes default permission classes and error handling.
+    All views inheriting from this class will automatically apply the error handling decorator.
+    """
     permission_classes = [IsAuthenticated]
 
+class GameCreateView(BaseAPIView):
+    """
+    Handles the creation of a new game instance.
+    This view receives a POST request to initialize a game with default values,
+    including players, board configuration (FEN), and game status. It validates
+    the input using the GameSerializer and saves the game if valid.
+    """
     def post(self, request, *args, **kwargs):
-
         game_data = {
             'red_player': None,
             'black_player': None,
@@ -33,9 +43,13 @@ class GameCreateView(APIView):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-class GameDetailView(APIView):
-    permission_classes = [IsAuthenticated]
-
+class GameDetailView(BaseAPIView):
+    """
+    Retrieves the details of a specific game.
+    This view handles GET requests for fetching the details of a game by its ID.
+    The game ID is decoded, and the game is retrieved from the database. If the
+    game is found, it returns the game data, otherwise an error is returned.
+    """
     def get(self, request, game_id, *args, **kwargs):
         decoded_id = hashids.decode(game_id)
         if not decoded_id:
@@ -47,12 +61,17 @@ class GameDetailView(APIView):
 
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-class GameUpdateView(APIView):
-    permission_classes = [IsAuthenticated]
-
+class GameUpdateView(BaseAPIView):
+    """
+    Updates an existing game instance.
+    This view handles PUT requests to update a game by its ID. The game data is
+    partially updated based on the provided input. The game ID is decoded, and
+    if valid, the corresponding game is updated.
+    """
     def put(self, request, game_id, *args, **kwargs):
         decoded_id = hashids.decode(game_id)
         if not decoded_id:
+
             return Response({'error': 'Invalid game ID'}, status=status.HTTP_404_NOT_FOUND)
 
         game = get_object_or_404(Game, id=decoded_id[0])
