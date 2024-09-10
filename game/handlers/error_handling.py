bf354633e45bbcc80f7517c6f6dd753e7f9f5b9f
Bilal Ahmad <bilal.ahmad02@A006-01020.local>
2024-09-10 19:07:07 +0500

    improved formatting and docstrings

--- game/handlers/error_handling.py
+++ game/handlers/error_handling.py
@@ -2,23 +2,42 @@ import functools
 
 
 class GameNotFoundError(Exception):
+    """
+    Exception raised when a game cannot be found.
+    """
     pass
 
 class PlayerNotFoundError(Exception):
+    """
+    Exception raised when a player cannot be found.
+    """
     pass
 
 class InvalidMoveError(Exception):
+    """
+    Exception raised when an invalid move is made in the game.
+    """
     pass
 
 class UnauthorizedError(Exception):
+    """
+    Exception raised when an unauthorized action is attempted.
+    """
     pass
 
 
 def exception_handler(func):
+    """
+     A decorator that wraps a function with exception handling logic.
+     If any of the predefined exceptions are raised within the wrapped function,
+     an appropriate error message will be sent to the consumer using the
+    send_error handler.
+    """
     @functools.wraps(func)
     async def wrapper(consumer, *args, **kwargs):
         from game.handlers.game_handler import send_error
         try:
+
             return await func(consumer, *args, **kwargs)
         except GameNotFoundError as e:
             await send_error(consumer, str(e))
--- game/middleware.py
+++ game/middleware.py
@@ -1,46 +1,49 @@
 from urllib.parse import parse_qs
+
 from channels.auth import AuthMiddlewareStack
 from channels.db import database_sync_to_async
 from django.conf import settings
-from users.models import CustomUser as User
 from django.contrib.auth.models import AnonymousUser
 from django.db import close_old_connections
-from jwt import InvalidSignatureError, ExpiredSignatureError, DecodeError
+from jwt import DecodeError, ExpiredSignatureError, InvalidSignatureError
 from jwt import decode as jwt_decode
 
-import logging
+from users.models import CustomUser as User
 
-logger = logging.getLogger(__name__)
+
 class JWTAuthMiddleware:
-    """Middleware to authenticate user for channels"""
-
+    """
+    Middleware to authenticate user for channels
+    """
     def __init__(self, app):
-        """Initialize the app."""
         self.app = app
 
     async def __call__(self, scope, receive, send):
         close_old_connections()
         try:
-            token = parse_qs(scope["query_string"].decode("utf8")).get('token', None)[0]
-            logger.info(f"Received token: {token}")
-
-            data = jwt_decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+            token = parse_qs(scope['query_string'].decode('utf8')).get('token', None)[0]
+            data = jwt_decode(token, settings.SECRET_KEY, algorithms=['HS256'])
             scope['user'] = await self.get_user(data['user_id'])
-            logger.info(f"Authenticated user: {scope['user']}")
-        except (TypeError, KeyError, InvalidSignatureError, ExpiredSignatureError, DecodeError) as e:
-            logger.error(f"Authentication failed: {str(e)}")
+
+        except (TypeError, KeyError, InvalidSignatureError, ExpiredSignatureError, DecodeError):
             scope['user'] = AnonymousUser()
 
         return await self.app(scope, receive, send)
 
     @database_sync_to_async
     def get_user(self, user_id):
-        """Return the user based on user ID."""
+        """
+        Return the user based on user ID.
+        """
         try:
+
             return User.objects.get(id=user_id)
         except User.DoesNotExist:
+
             return AnonymousUser()
 
 def JWTAuthMiddlewareStack(app):
-    """This function wraps channels authentication stack with JWTAuthMiddleware."""
+    """
+    This function wraps channels authentication stack with JWTAuthMiddleware.
+    """
     return JWTAuthMiddleware(AuthMiddlewareStack(app))
