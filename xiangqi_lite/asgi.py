ae2a4a2658866e788ce4852bd28654ca156c388e
Bilal Ahmad <bilal.ahmad02@A006-01020.local>
2024-09-10 19:09:21 +0500

    improved spacing

--- xiangqi_lite/asgi.py
+++ xiangqi_lite/asgi.py
@@ -1,7 +1,8 @@
 import os
-from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
+
 import django
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'xiangqi_lite.settings')
 django.setup()
