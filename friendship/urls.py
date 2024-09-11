b1e8fb6711188f5d6412a111501b2a14cea14436
Bilal Ahmad <bilal.ahmad02@arbisoft.com>
2024-09-11 13:34:57 +0500

    improved docstrings and refactored urls.py for friendship app

--- friendship/urls.py
+++ friendship/urls.py
@@ -1,19 +1,19 @@
 from django.urls import path
-from .views import (
-    SendFriendRequestView,
-    AcceptFriendRequestView,
-    RejectFriendRequestView,
+
+from friendship.views import (
+    AcceptRejectFriendRequestView,
+    FriendRequestsView,
+    ListFriendsView,
     ListSentFriendRequestsView,
-    ListFriendsView, FriendRequestsView,SearchUsersView
+    SearchUsersView,
+    SendFriendRequestView,
 )
 
 urlpatterns = [
     path('send-request/', SendFriendRequestView.as_view(), name='send-friend-request'),
-    path('accept-request/', AcceptFriendRequestView.as_view(), name='accept-friend-request'),
-    path('reject-request/', RejectFriendRequestView.as_view(), name='reject-friend-request'),
+    path('repond-request/', AcceptRejectFriendRequestView.as_view(), name='accept-friend-request'),
     path('sent-requests/', ListSentFriendRequestsView.as_view(), name='list-sent-friend-requests'),
     path('friends/', ListFriendsView.as_view(), name='list-friends'),
     path('requests/', FriendRequestsView.as_view(), name='friend-requests'),
     path('users/search/', SearchUsersView.as_view(), name='search_users'),
 ]
-
