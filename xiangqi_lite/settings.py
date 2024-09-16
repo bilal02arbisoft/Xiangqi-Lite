1b5e782a093d1f4b9ca211c86edab8b017accb35
bilal02arbisoft <bilal.ahmad02@arbisoft.com>
2024-09-16 13:21:31 +0500

    cache settings

--- xiangqi_lite/settings.py
+++ xiangqi_lite/settings.py
@@ -129,6 +129,15 @@ TEMPLATES = [
         },
     },
 ]
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
 
 DATABASES = {
     'default': {
